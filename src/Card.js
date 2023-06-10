c3d540a679b519a9851ecf2dc07dc5a99393186b
belousovigorvic <68902284+belousovigorvic@users.noreply.github.com>
2023-06-10 21:47:52 +0600

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ const Card = (props) => {
             <img className='card__img' src={props.img} alt="img"/>
             <h1 className="card__title">{props.title}</h1>
             <p className="card__sub-title">{props.subTitle}</p>
-            <button className='card__button'>{props.nameBtn}</button>
+            <button onClick={props.log} className='card__button'>{props.nameBtn}</button>
         </div>
     )
 }
